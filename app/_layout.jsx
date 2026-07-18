a5f79569bef8c144f1e8f3b1119d98f9b6b9577f
Basher-kap <kap.bash@gmail.com>
2026-07-18 15:00:26 +0800

    chore: update dependencies to newer versions and adjust package versions for compatibility

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -3,15 +3,13 @@ import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import { Platform } from 'react-native';
 import * as NavigationBar from 'expo-navigation-bar';
-import { AppMetrics, AppMetricsRoot } from 'expo-observe';
 import UpdateBar from '../components/UpdateBar';
 
-function RootLayout() {
+export default function RootLayout() {
   useEffect(() => {
     if (Platform.OS === 'android') {
       NavigationBar.setVisibilityAsync('hidden');
     }
-    AppMetrics.markInteractive();
   }, []);
 
   return (
@@ -22,5 +20,3 @@ function RootLayout() {
     </>
   );
 }
-
-export default AppMetricsRoot.wrap(RootLayout);
